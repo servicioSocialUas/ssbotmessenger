59d45ae93dd9aead59a52ae85745efbbbdf426cc
ConsorcioPrestamos <consorcio.prestamos.web@gmail.com>
2018-08-24 10:41:28 -0600

    7

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports={
     token_facebook : 'EAAGy44sxyPkBAPHomAhXtFy5U5hNTYe3uvsHy6gRfiQLp1QyUIAGqKwZCCdVRrlylRvPKZAjEkhrMMEJjeYC5ZBgPthLlJgeKO1ZBHQC3xGbiSsysoykMT65EZAmFrZAL0G6BN3CVfj9e6uvJEEXZBNLyZBZCLeyK1xzI9HgcHv1HGfFZAfGy09DCb',
     idapp_facebook : '478165339326713',
-    port: process.env.port || 3000,
+    port: process.env.PORT || 3000,
     token_bot: 'token_de_servicio_social'
 }
